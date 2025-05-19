d5c0f55cd2dfd7a1e74fc9c19d23e111b27f96b2
Alan V <alan_nerde@hotmail.com>
2025-05-19 16:55:55 -0300

    adicionado arquivos individuais para swagger

--- src/app.ts
+++ src/app.ts
@@ -2,13 +2,14 @@ import cors from 'cors';
 import express from 'express';
 import swaggerUi from 'swagger-ui-express';
 import routes from '../src/routes/Index';
-import swaggerSpec from './swagger';
+import { getSwaggerSpec } from './docs/swagger';
 
 const app = express();
-app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-app.use(cors());
 app.use(express.json());
-app.use('/api', routes);
+app.use(cors());
 
+const swaggerDocument = getSwaggerSpec();
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use('/api', routes);
 
 export default app;
---
+++ src/docs/swagger.ts
@@ -0,0 +1,26 @@
+import path from 'path';
+import YAML from 'yamljs';
+
+export function getSwaggerSpec() {
+  const productDocument = YAML.load(path.join(__dirname, './product.yaml'));
+  const customerDocument = YAML.load(path.join(__dirname, './customer.yaml'));
+  return {
+    openapi: '3.0.0',
+    info: {
+      title: 'TESTE SWAGGER',
+      version: '1.0.0',
+      description: 'DESCRICAO DENTRO DE SWAGGER.TS',
+    },
+    servers: [{ url: 'http://localhost:3000/api' }],
+    paths: {
+      ...productDocument.paths,
+      ...customerDocument.paths,
+    },
+    components: {
+      schemas: {
+        ...productDocument.components.schemas,
+        ...customerDocument.components.schemas,
+      },
+    },
+  };
+}
